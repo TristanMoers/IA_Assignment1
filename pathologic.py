d558bce1571a3d786df1e0e47a5a7bd4bb60b407
TristanMoers <tristan.moers@hotmail.com>
2018-10-02 16:19:53 +0200

    first commit

---
+++ pathologic.py
@@ -0,0 +1,69 @@
+# -*-coding: utf-8 -*
+'''NAMES OF THE AUTHOR(S): Gaël Aglin <gael.aglin@uclouvain.be>, Francois Aubry <francois.aubry@uclouvain.be>'''
+from search import *
+
+
+#################
+# Problem class #
+#################
+class Pathologic(Problem):
+
+    def successor(self, state):
+        pass
+
+
+###############
+# State class #
+###############
+
+class State:
+    def __init__(self, grid):
+        self.nbr = len(grid)
+        self.nbc = len(grid[0])
+        self.grid = grid
+
+    def __str__(self):
+        s = ""
+        for i in range(0, self.nbr):
+            for j in range(0, self.nbc):
+                s = s + str(self.grid[i][j]) + " "
+            s = s.rstrip()
+            if i < self.nbr - 1:
+                s = s + '\n'
+        return s
+
+
+
+######################
+# Auxiliary function #
+######################
+def readInstanceFile(filename):
+    lines = [line.replace(" ","").rstrip('\n') for line in open(filename)]
+    n = len(lines)
+    m = len(lines[0])
+    grid_init = [[lines[i][j] for j in range(0, m)] for i in range(0, n)]
+    return grid_init
+
+
+#####################
+# Launch the search #
+#####################
+
+grid_init = readInstanceFile(sys.argv[1])
+init_state = State(grid_init)
+
+problem = Pathologic(init_state)
+
+# example of bfs graph search
+node = breadth_first_graph_search(problem)
+
+# example of print
+path = node.path()
+path.reverse()
+
+
+
+print('Number of moves: ' + str(node.depth))
+for n in path:
+    print(n.state)  # assuming that the __str__ function of state outputs the correct format
+    print()
