821dc3f452f36c01a349cf2b55124d2ccafaf11d
TristanMoers <tristan.moers@hotmail.com>
2018-10-03 16:31:17 +0200

    successor+test_goal ok

--- pathologic.py
+++ pathologic.py
@@ -1,6 +1,7 @@
 # -*-coding: utf-8 -*
 '''NAMES OF THE AUTHOR(S): Gaël Aglin <gael.aglin@uclouvain.be>, Francois Aubry <francois.aubry@uclouvain.be>'''
 from search import *
+import time
 
 
 #################
@@ -12,19 +13,49 @@ class Pathologic(Problem):
         actions = list()
         xb = state.x
         yb = state.y
+        #newgrid = [x[:] for x in state.grid]
 
         ########
         #UP
-        if(yb>0 and grid[y-1] != '1')
+        if(yb>0 and state.grid[yb-1][xb] != '1' and state.grid[yb-1][xb] != 'x'):
+            newgrid = [x[:] for x in state.grid]
+            newgrid[yb-1][xb] = '$'
+            newgrid[yb][xb] = 'x'
+            if state.grid[yb-1][xb] == '_':
+                actions.append(('up', State(newgrid, xb, yb-1, state.counter+1)))
+            else:
+                actions.append(('up', State(newgrid, xb, yb-1, state.counter)))
 
         #DOWN
-        if(yb<state.nbc-1 and grid[yb+1] != '1')
+        if(yb<state.nbc-1 and state.grid[yb+1][xb] != '1' and state.grid[yb+1][xb] != 'x'):
+            newgrid = [x[:] for x in state.grid]
+            newgrid[yb+1][xb] = '$'
+            newgrid[yb][xb] = 'x'
+            if state.grid[yb+1][xb] == '_':
+                actions.append(('down', State(newgrid, xb, yb+1, state.counter+1)))
+            else:
+                actions.append(('down', State(newgrid, xb, yb+1, state.counter)))
 
         #LEFT
-        if(xb>0 and grid[x-1] != '1')
+        if(xb>0 and state.grid[yb][xb-1] != '1' and state.grid[yb][xb-1] != 'x'):
+            newgrid = [x[:] for x in state.grid]
+            newgrid[yb][xb-1] = '$'
+            newgrid[yb][xb] = 'x'
+            if state.grid[yb][xb-1] == '_':
+                actions.append(('left', State(newgrid, xb-1, yb, state.counter+1)))
+            else:
+                actions.append(('left', State(newgrid, xb-1, yb, state.counter)))
 
         #RIGHT
-        if(xb<state.nbr-1 and grid[xb+1] != '1')
+        if(xb<state.nbr-1 and state.grid[yb][xb+1] != '1' and state.grid[yb][xb+1] != 'x'):
+            newgrid = [x[:] for x in state.grid]
+            newgrid[yb][xb+1] = '$'
+            newgrid[yb][xb] = 'x'
+            if state.grid[yb][xb+1] == '_':
+                actions.append(('right', State(newgrid, xb+1, yb, state.counter+1)))
+            else:
+                actions.append(('right', State(newgrid, xb+1, yb, state.counter)))
+
 
         for a in actions:
             yield a
@@ -82,12 +113,12 @@ xb = 0
 yb = 0
 goal = 0
 
-for x in range(0, len(grid_init)):
-    for y in range(0, len(grid_init[x])):
-        if grid_init[x][y] == '$':
+for y in range(0, len(grid_init)):
+    for x in range(0, len(grid_init[y])):
+        if grid_init[y][x] == '$':
             xb = x
             yb = y
-        if grid_init[x][y] == '_':
+        if grid_init[y][x] == '_':
             goal = goal+1
 
 # ======================================
@@ -98,11 +129,13 @@ problem = Pathologic(init_state)
 problem.goal = goal
 
 # example of bfs graph search
+start_time = time.time()
 node = breadth_first_graph_search(problem)
 
 # example of print
 path = node.path()
 path.reverse()
+end_time = time.time()
 
 
 
@@ -110,3 +143,5 @@ print('Number of moves: ' + str(node.depth))
 for n in path:
     print(n.state)  # assuming that the __str__ function of state outputs the correct format
     print()
+
+print("Finished in %.2f seconds" % (end_time - start_time))
